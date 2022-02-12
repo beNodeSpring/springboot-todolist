c361bde1ea34bd48701edca95219fccf6afafb6d
ds2lvg <ds2lvg@gmail.com>
2022-02-12 13:59:51 +0900

    회원가입/로그인 단순 API 작업(스프링시큐리티 적용전)

---
+++ src/main/java/com/todolist/demo/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.todolist.demo.controller;
+
+import com.todolist.demo.dto.ResponseDTO;
+import com.todolist.demo.dto.UserDTO;
+import com.todolist.demo.model.UserEntity;
+import com.todolist.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        try {
+            // body로 넘어온 값으로 저장할 유저 생성
+            UserEntity user = UserEntity.builder()
+                    .email(userDTO.getEmail())
+                    .username(userDTO.getUsername())
+                    .password(userDTO.getPassword())
+                    .build();
+            // 유저 저장
+            UserEntity registeredUser =userService.create(user);
+            // 저장된 유저값을 UserDTO 리턴(유저 정보는 항상 하나이므로 List인 ResponseDTO를 사용안함)
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .email(registeredUser.getEmail())
+                    .id(registeredUser.getId())
+                    .username(registeredUser.getUsername())
+                    .password(registeredUser.getPassword())
+                    .build();
+            return ResponseEntity.ok(responseUserDTO);
+        } catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
+        // 이메일 패스워드 검증
+        UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
+        if(user != null) {
+            // 토큰 생성
+            final UserDTO responseUserDTO = UserDTO.builder()
+                    .email(user.getEmail())
+                    .username(user.getUsername())
+                    .password(user.getPassword())
+                    .build();
+            return ResponseEntity.ok().body(responseUserDTO);
+        } else {
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error("Login failed.")
+                    .build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+}
---
+++ src/main/java/com/todolist/demo/model/UserEntity.java
@@ -0,0 +1,31 @@
+package com.todolist.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class UserEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false) // null이 가능하게 해야 페이스북등의 auth2 사용 가능
+    private String password;
+}
